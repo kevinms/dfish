84aeb81f5adacabc0141348345631700b41ec133
kevin4@g.clemson.edu <kevin4@g.clemson.edu@2e1292fe-2802-11df-8847-f3d5e5ef3dde>
2010-03-05 04:59:36 +0000

    test-view added

---
+++ trunk/src/test-view.c
@@ -0,0 +1,25 @@
+#include "TinyCurses.h"
+
+#define MSIZE 100
+int main (int argc, char *argv[])
+{
+	char ch = 'a';
+	int i, j;
+	char map[MSIZE][MSIZE];
+	for(i = 0; i < MSIZE; i++)
+		for(j = 0; j < MSIZE; j++)
+			map[i][j] = 'a';
+	
+	//printw("Curses!\n");
+	for(; ch < 'z'; ch++) {
+		move(0,0);
+		
+		for(i = 0; i < MSIZE; i++)
+			for(j = 0; j < MSIZE; j++)
+				printw("%c",ch);
+		refresh();
+		//getch();
+	}
+	
+	return 0;
+};
