50bacb89f95564541cd148ac3594466b2628f85f
nsmith@clemson.edu <nsmith@clemson.edu@2e1292fe-2802-11df-8847-f3d5e5ef3dde>
2010-03-07 05:28:52 +0000

    implemented basic scrolling, only does bottom line, and segfaults alot, will take care of later

--- trunk/src/map.h
+++ trunk/src/map.h
@@ -8,17 +8,20 @@
 
 #include "TinyCurses.h"
 
-#define lLDefault 150
-#define wLDefault 200
-#define lGDefault 150
-#define wGDefault 200
+#define lLDefault 20
+#define wLDefault 30
+#define lGDefault 200
+#define wGDefault 250
 
 //Positioning System struct
 struct posSys_t {
 	int lSize;
 	int wSize;
 	int **map;
+	int xPos;
+	int yPos;
 };
 
 struct posSys_t *init_map (int lSize, int wSize, int setup);
-struct posSys_t *move_map (struct posSys_t *GPS, struct posSys_t *LPS);
+struct posSys_t *fix_map (struct posSys_t *GPS, struct posSys_t *LPS);
+struct posSys_t *move_map (int keyPressed, struct posSys_t *GPS, struct posSys_t *LPS);
