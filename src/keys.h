3eb8132a8203a40ebdd80be4879879c7b16a74ba
nsmith <nsmith@clemson.edu>
2010-03-08 06:22:22 +0000

    added keys.[ch] and test-poll.c\nthese test out SDL's polling for input events and switch case scenarios\n it works

---
+++ src/keys.h
@@ -0,0 +1,14 @@
+/***************************************************
+ * keys.h
+ * 
+ * Inits functions for handling keyboard events
+ ***************************************************/
+
+#include "map.h"
+
+
+void upKeyEvent (SDL_Event *event, struct posSys_t *GPS, struct posSys_t *LPS);
+
+void downKeyEvent (SDL_Event *event, struct posSys_t *GPS, struct posSys_t *LPS);
+
+void shiftKeyEvent (SDL_Event *event, struct posSys_t *GPS, struct posSys_t *LPS);
