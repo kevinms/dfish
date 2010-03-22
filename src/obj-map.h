4522e80e2a5fdaaaaeef0e3d8171f4bebe48fb41
plecroy <plecroy@g.clemson.edu>
2010-03-22 22:03:04 +0000

    Nick fixed something in obj-map.h and I've got 4 example planets in dataz. Rename whichever one to .dat to render with test-poll.  I've also commented out the letters and grid.

--- src/obj-map.h
+++ src/obj-map.h
@@ -17,13 +17,13 @@
 /***			Define  space stuff to be 0-19			***/
 #define OBJ_STAR		0
 #define OBJ_PLANET		1
-#define OBJ_ASTEROID	2
+#define OBJ_ASTEROID		2
 #define OBJ_NEBULA		3
 
 /***	Define different types of units to be 20-49		***/
 #define OBJ_UNIT		20
 #define OBJ_ENEMY		21
-#define OBJ_CREATURE	22
+#define OBJ_CREATURE		22
 
 /***	Define different types of items to be 50-99		***/
 #define OBJ_ITEM		50
