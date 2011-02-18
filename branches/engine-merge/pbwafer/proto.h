ae0c5fea1a7e9e3ab8a55cb299114d2d55c7f4dd
kevin4@g.clemson.edu <kevin4@g.clemson.edu@2e1292fe-2802-11df-8847-f3d5e5ef3dde>
2011-02-18 21:41:09 +0000

    moved to a fixed width buffer for reliable packets and fixed the reliability layer and got seq_num wrap around working

--- branches/engine-merge/pbwafer/proto.h
+++ branches/engine-merge/pbwafer/proto.h
@@ -2,12 +2,12 @@
 #define __PROTO_H
 
 // PROTO Packet Header
-// +--------+--------+---------+------+--------+----------+------+------+
-// |  UDP   | Packet | seq_num | Host |  ack   |   ack    | Type | Data |
-// | Header |  Size  |         |  ID  |        | bitfield |      |      |
-// +--------+--------+---------+------+--------+----------+------+------+
-//     8         2        2        1      2         4        n
-// 20(!16) bytes of header data including UDP header
+// +--------+---------+------+--------+----------+------+------+
+// | Packet | seq_num | Host |  ack   |   ack    | Type | Data |
+// |  Size  |         |  ID  |        | bitfield |      |      |
+// +--------+---------+------+--------+----------+------+------+
+//     2         2        1      2         4        n
+// 11 bytes of header data ( +8 for UDP header)
 
 // To understand some of this code figure out who you are.  Are you the client
 // or the server?  Whoever you are, your buddy is the opposite.
@@ -45,8 +45,11 @@
 #define STATE_WAIT_ON_CON  2
 #define STATE_CON          3
 
-#define TIMEOUT   1000
+#define TIMEOUT   3
 #define DROP_TIME 1500
+
+#define NUMBITS 32
+#define MAX_SHORT 65535
 
 // Holds all header info
 typedef struct header_s
@@ -59,7 +62,6 @@ typedef struct header_s
 } header_t;
 
 // Used to store a copy of a packet to possibly resend later
-//TODO: add a timer to this packet
 typedef struct packet_s
 {
 	unsigned short seq_num;
@@ -75,7 +77,9 @@ typedef struct hostinfo_s
 
 	pwtimer_t keepalive;
 
-	list_t *unacked_reliable_packets; //TODO: change to hashmap
+	packet_t *unacked_reliable_packet_map[NUMBITS];
+	unsigned short latest_ack_seen;
+
 	list_t *packet_queue; // Maybe use this when implementing flow control
 } hostinfo_t;
 
@@ -121,6 +125,10 @@ void PROTO_handle_timeout(hostinfo_t *h);
 void PROTO_queue_packet(hostinfo_t *h, packet_t *p);
 void PROTO_print_header_t(header_t *h);
 void PROTO_print_header(unsigned char *buf);
+
+// Handles sequence number and ack wrap-around
+int PROTO_more_recent(unsigned short s1, unsigned short s2, unsigned short max_sequence);
+unsigned short PROTO_diff(unsigned short s1, unsigned short s2, unsigned short max_sequence);
 
 // Used by client
 void PROTO_set_clientinfo(int state, char *name);
