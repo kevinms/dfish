25b8147a49c29e9f0a4d17652adc14a1808a0ae6
kevin4@g.clemson.edu <kevin4@g.clemson.edu@2e1292fe-2802-11df-8847-f3d5e5ef3dde>
2011-03-12 23:00:42 +0000

    deleted early engine branch that is no longer used

--- branches/kevin4-pbwafer/log.h
+++
@@ -1,6 +0,0 @@
-#ifndef __LOG_H
-#define __LOG_H
-
-void LOG_Error();
-
-#endif /* !__LOG_H */
--- branches/kevin4-pbwafer/timer.h
+++
@@ -1,8 +0,0 @@
-#ifndef __TIMER_H
-#define __TIMER_H
-
-typedef struct pw_timer_s {
-	
-} pw_timer_t;
-
-#endif /* !__TIMER_H */
